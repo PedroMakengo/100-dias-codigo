85f0426706c2009959ec0bc6b616e76057cdf719
PedroMakengo <pedromakengomiguel@gmail.com>
2023-05-19 14:12:31 +0100

    Primeiro commit do meu trabalho

---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import './globals.css'
+import { ReactNode } from 'react'
+import {
+  Roboto_Flex as Roboto,
+  Bai_Jamjuree as BaiJamjuree,
+} from 'next/font/google'
+
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['100', '300', '500', '700', '900'],
+  variable: '--font-roboto',
+})
+const baiJamjuree = BaiJamjuree({
+  subsets: ['latin'],
+  weight: '700',
+  variable: '--font-bai-jamjuree',
+})
+
+export const metadata = {
+  title: 'NLW Spacetime',
+  description:
+    'Uma cápsula do tempo construída com React, NextJs, Tailwind e Typescript.',
+}
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${roboto.variable} ${baiJamjuree.variable} bg-gray-900 font-sans text-gray-100`}
+      >
+        {children}
+      </body>
+    </html>
+  )
+}
